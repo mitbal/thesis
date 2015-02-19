806f2c664d3879adc2e659b2696494b10a225ea8
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2015-02-19 18:51:47 +0100

    Added init file to initialize file and variable

---
+++ init.m
@@ -0,0 +1,10 @@
+% Variable and definition declaration
+
+global VOC07PATH VOCCLASS;
+
+VOC07PATH = '/home/iqbal/datasets/VOCdevkit/VOC2007/';
+VOCCLASS = {'aeroplane' 'bicycle' 'bird' 'boat' 'bottle' 'bus' 'car' 'cat' 'chair' 'cow' 'diningtable' 'dog' 'horse' 'motorbike' 'person' 'pottedplant' 'sheep' 'sofa' 'train' 'tvmonitor'};
+
+
+% Path declaration
+addpath('/home/iqbal/datasets/VOCdevkit/VOCcode');
