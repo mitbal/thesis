47f3d10a00b50edce45fa35bcb06b4b65cc756eb
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2015-03-26 19:35:04 +0100

    Print detection for each classes

--- print_detection.m
+++ print_detection.m
@@ -2,60 +2,66 @@
 init;
 
 % Get test image path
-testing_data = 'test';
+testing_data = exp_params.test_dataset;
 im_dir = [VOC07PATH 'JPEGImages/'];
 fid = fopen([VOC07PATH 'ImageSets/Main/' testing_data '.txt']);
-test_imgs = textscan(fid, '%s');
-test_imgs = test_imgs{1};
+imgs = textscan(fid, '%s');
+imgs = imgs{1};
 
 % Load trained SVM models
-net_model = 'caffenet';
-training_data = 'trainval';
+net_model = exp_params.net_model;
+training_data = exp_params.dataset;
 load(['svm_models/' net_model '/' training_data '.mat']);
 
-num_files = 496;
-img_per_file = 10;
-for ii=1:1
-    disp(['Class ' VOCCLASS{ii}]);
-    fid = fopen(['results/' net_model '_' training_data '_' testing_data '_' VOCCLASS{ii} '.txt'], 'w');
-    model = models{ii};
-    for jj=1:num_files
-        filename = ['data/' net_model '/' testing_data '_' num2str(jj) '.mat'];
-        disp(filename);
-        load(filename);
-        num_test = size(tests, 1);
-        for kk=1:num_test
-            disp(['Image ' num2str(kk)]);
-            test = tests{kk};
-            num_boxes = size(test, 1);
-            features = zeros(num_boxes, 4096);
-            boxes = zeros(num_boxes, 4);
-            for bb=1:num_boxes
-                features(bb, :) = test{bb}.feature';
-                boxes(bb, :) = test{bb}.bbox;
-            end
-            features = features .* range;
-            
-            prediction = model.w * features';
-            prediction = [-prediction; 1:num_boxes]';
-            prediction = sortrows(prediction, 1);
-            
-            if -prediction(1, 1) > 0
-                scores = -prediction(:, 1);
-                index = scores > 0;
-                scores = scores(index);
-                dets = boxes(prediction(index, 2), :);
+% Open stream to file
+fids = cell(20,1);
+for cls=1:20
+    fid = fopen(['results/' net_model '_' training_data '_' testing_data '_' VOCCLASS{cls} '.txt'], 'w');
+    fids{cls} = fid;
+end
 
-                [pruned_boxes, pruned_scores] = prune_detection(dets, scores);
-                for bb=1:size(pruned_boxes, 1)
-                    id = test_imgs{(jj-1)*img_per_file + kk};
-                    box = pruned_boxes(bb, :);
-                    line = [id ' ' num2str(pruned_scores(bb), 6) ' ' num2str(box(2)) ' ' num2str(box(1)) ' ' num2str(box(4)) ' ' num2str(box(3)) '\n'];
-                    disp(line);
-                    fprintf(fid, line);
-                end
+num_imgs = size(imgs, 1);
+for ii=1:num_imgs
+    tic
+    disp(['Detecting image: ' imgs{ii} ' ' num2str(ii) '/' num2str(num_imgs)]);
+    filename = ['data/' net_model '/' testing_data '/' imgs{ii} '.mat'];
+    load(filename);
+    
+    num_regions = size(regions, 2);
+    test_features = zeros(num_regions, 4096);
+    boxes = zeros(num_regions, 4);
+    for bb=1:num_regions
+        test_features(bb, :) = features{bb}';
+        boxes(bb, :) = regions{bb};
+    end
+    test_features = test_features .* range;
+    
+    for cls=1:1
+        model = models{cls};
+
+        prediction = model.w * test_features';
+        prediction = [-prediction; 1:num_regions]';
+        prediction = sortrows(prediction, 1);
+
+        if -prediction(1, 1) > 0
+            scores = -prediction(:, 1);
+            index = scores > 0;
+            scores = scores(index);
+            dets = boxes(prediction(index, 2), :);
+
+            [pruned_boxes, pruned_scores] = prune_detection(dets, scores);
+            for bb=1:size(pruned_boxes, 1)
+                id = imgs{ii};
+                box = pruned_boxes(bb, :);
+                line = [id ' ' num2str(pruned_scores(bb), 6) ' ' num2str(box(2)) ' ' num2str(box(1)) ' ' num2str(box(4)) ' ' num2str(box(3)) '\n'];
+                disp(line);
+                fprintf(fids{cls}, line);
             end
         end
     end
-    fclose(fid);
+    toc
+end
+
+for cls=1:20
+    fclose(fids{cls});
 end
