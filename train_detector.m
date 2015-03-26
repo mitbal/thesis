14444fe736f58351465d5a71b111342bf2642c0b
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2015-03-26 15:06:14 +0100

    Hard negative mining version removing easy samples, v0.1, probably wrong

--- train_detector.m
+++ train_detector.m
@@ -1,80 +1,67 @@
 % Train SVM detector
-clear all;
 init;
 
-net_model = 'caffenet';
-pos_dataset = 'trainval';
-dataset = 'train';
+net_model = exp_params.net_model;
+dataset = exp_params.dataset;
 
-% Load positive data
-pos = load(['data/' net_model '/' pos_dataset '_positive.mat']);
+%range = get_norm_factor();
+%range = (1/170);
+range = 1;
+
+% First, load the positive samples
+pos = load(['data/' net_model '/' dataset '/positive.mat']);
 num_pos = size(pos.labels, 2);
 
-% Load the first negative data
-neg = load(['data/' net_model '/' dataset '_negative_1.mat']);
-num_neg = size(neg.features, 2);
+fid = fopen([VOC07PATH 'ImageSets/Main/' dataset '.txt']);
+imgs = textscan(fid, '%s');
+imgs = imgs{1};
+num_imgs = length(imgs);
 
-% Construct 1 big matrix
-num_train = num_pos+num_pos;
-features = zeros(num_train, 4096);
+pos_features = zeros(num_pos, 4096);
 for ii=1:num_pos
-    features(ii, :) = pos.features{ii}';
+    pos_features(ii, :) = pos.features{ii}';
 end
-for ii=1:num_pos
-    features(ii+num_pos, :) = neg.features{ii}';
+pos_labels = cell2mat(pos.labels)';
+
+% Normalize
+pos_features = pos_features .* range;
+
+% Initialize the cache
+train_cache = cell(20, 1);
+for cls=1:20
+    pos_index = (pos_labels==cls);
+    neg_index = (pos_labels~=cls);
+    train_cache{cls}.pos = pos_features(pos_index, :);
+    train_cache{cls}.neg = pos_features(neg_index, :);
 end
-labels = cell2mat(pos.labels);
-labels = [labels'; zeros(num_pos, 1)];
 
-% Normalization??? Skip for now
+pos_weight = 2;
+penalty = 0.001;
+mode = 3;
 
-
-% Train detector for each class
-models = cell(20,1);
-for ii=1:20
-    index = labels == ii;
-    num_pos = sum(index);
-    num_neg = num_pos;
-    neg_features = features(labels==0, :);
-    features_cache = [features(index, :); neg_features(1:num_pos, :)];
-    train_labels = [ones(sum(index), 1); -ones(num_pos, 1)];
-    
-    disp(['Training class: ' num2str(ii)]);
-    train_features = sparse(features_cache);
-    options = ['-w1 ' num2str(2) ' -c ' num2str(0.001) ' -s 2']; % Parameters taken from rcnn code
-    model = train(train_labels, train_features, options);
-    
-    scores = model.w * train_features';
-    prediction = scores > 0;
-    prediction = prediction .* 2 -1;
-    prediction = prediction';
-    tp = sum(prediction(1:num_pos)==train_labels(1:num_pos));
-    tn = sum(prediction(num_pos+1:end)==train_labels(1+num_pos:end));
-    disp(['tp ' num2str(tp) '/' num2str(num_pos)]);
-    disp(['tn ' num2str(tn) '/' num2str(num_neg)]);
-    
-    for jj=1:6
-        disp(['Load the ' num2str(jj) ' negative data']);
-        neg = load(['data/' net_model '/' dataset '_negative_' num2str(jj) '.mat']);
-        num_neg = size(neg.features, 2);
-        neg_features = zeros(num_neg, 4096);
-        for kk=1:num_neg
-            neg_features(kk, :) = neg.features{kk}';
-        end
+% Train for each image
+for ii=1:num_imgs
+    disp(['Training with data from image: ' imgs{ii} ' ' num2str(ii) '/' num2str(num_imgs)]);
+    train_data = load(['data/' net_model '/' dataset '/' imgs{ii} '.mat']);
+    num_data = size(train_data.labels, 2);
+    data_labels = cell2mat(train_data.labels);
+    data_features = zeros(num_data, 4096);
+    for jj=1:num_data
+        data_features(jj, :) = train_data.features{jj}';
+    end
+    for cls=1:20
+        % Prepare training data
+        disp(['Training class: ' VOCCLASS{cls} ' ' num2str(cls) '/20']);
+        neg_index = (data_labels == 0);
+        neg_features = data_features(neg_index, :);
+        neg_features = neg_features .* range;
         
-        % Get misclassified data
-        % should be normalized first
-        scores = model.w * neg_features';
-        index = scores > 0;
-        num_wrong = sum(index);
-        disp(['wrong ' num2str(num_wrong)]);
+        num_neg = size(train_cache{cls}.neg, 1);
+        num_pos = size(train_cache{cls}.pos, 1);
         
-        features_cache = [features_cache; neg_features(index, :)];
-        train_features = sparse(features_cache);
-        train_labels = [train_labels; -ones(num_wrong, 1)];
-        num_neg = num_neg + num_wrong;
-        
-        % Retrain
+        train_features = sparse([train_cache{cls}.pos; train_cache{cls}.neg]);
+        train_labels = [ones(num_pos, 1); -ones(num_neg, 1)];
+        options = ['-w1 ' num2str(pos_weight) ' -c ' num2str(penalty) ' -s ' mode];
         model = train(train_labels, train_features, options);
         
         % Evaluate
@@ -85,10 +72,25 @@ for ii=1:20
         tp = sum(prediction(1:num_pos)==train_labels(1:num_pos));
         tn = sum(prediction(num_pos+1:end)==train_labels(1+num_pos:end));
         disp(['tp ' num2str(tp) '/' num2str(num_pos)]);
-        disp(['tn ' num2str(tn) '/' num2str(size(train_labels,1)-num_pos)]);
+        disp(['tn ' num2str(tn) '/' num2str(num_neg)]);
+        
+        % Update train_cache
+        % First shrink the cache
+        scores = model.w * train_cache{cls}.neg';
+        not_easy_samples = train_cache{cls}.neg(scores > -1, :);
+        num_easy = size(train_cache{cls}.neg, 1) - size(not_easy_samples,1);
+        train_cache{cls}.neg = not_easy_samples;
+        disp(['Number of easy samples ' num2str(num_easy)]);
+        
+        % Then grow the cache by adding new hard negative from this dataset
+        scores = model.w * neg_features';
+        hard_samples = neg_features(scores > -1, :);
+        train_cache{cls}.neg = [train_cache{cls}.neg; hard_samples];
+        disp(['Number of hard samples ' num2str(size(hard_samples, 1))]);
+        
+        % Save the model for every iteration, so it can continue if
+        % interrupted
+        models{cls} = model;
+        save(['svm_models/' net_model '/' dataset '.mat'], 'models', 'range', '-v7.3');
     end
-    
-    models{ii} = model;
-    save(['svm_models/' net_model '/' dataset '.mat'], 'models', '-v7.3');
 end
-
